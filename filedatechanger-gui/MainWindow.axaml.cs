7669499ede85d0eee73e9539ea922458fb8c326b
StarCpt <86216339+StarCpt@users.noreply.github.com>
2023-05-30 21:17:59 -0700

    Add project files.

---
+++ filedatechanger-gui/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace filedatechanger_gui
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
