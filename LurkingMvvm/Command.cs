93b4c642553fd683950943bf696712724d336bd5
StarCpt <86216339+StarCpt@users.noreply.github.com>
2023-06-01 12:02:29 -0700

    create lurkingmvvm project

---
+++ LurkingMvvm/Command.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+
+namespace LurkingMvvm
+{
+    public class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        readonly Action<object?> executeCallback;
+
+        public Command(Action<object?> executeCallback)
+        {
+            this.executeCallback = executeCallback;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public virtual bool CanExecute(object? parameter) => true;
+        public virtual void Execute(object? parameter) => executeCallback.Invoke(parameter);
+    }
+}
---
+++ LurkingMvvm/NotifyPropertyChangedBase.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LurkingMvvm
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void PreviewPropertyChanged(PropertyChangedEventArgs e)
+        {
+
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            var args = new PropertyChangedEventArgs(propertyName);
+            PreviewPropertyChanged(args);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetValue<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                RaisePropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
